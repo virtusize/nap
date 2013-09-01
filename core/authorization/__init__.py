32e30c6d6d98f1f690fbcda26a687d4d29021426
Jevgenij Tsoi <jevgenij@virtusize.com>
2013-09-01 22:17:52 +0200

    Additional thoughts on auth

--- core/authorization/__init__.py
+++ core/authorization/__init__.py
@@ -1,6 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+"""
+Taking inspiration from rails and other frameworks:
+
+
+I would like to have something like this in the business layer:
+
+ModelGuard():
+
+    guest = Role()
+    guast.can(Action.read, AnyModel)
+
+    admin = Role()
+    admin.can(Actions.manage, Store)
+
+    store_owner = Role()
+    store_owner.can(Actions.manage, Store, if=is_store_owner)
+
+
+guard = ModelGuard()
+
+guard.can(user, Actions.read, model) --> True/False
+
+"""
+
 
 class Identity:
     """
