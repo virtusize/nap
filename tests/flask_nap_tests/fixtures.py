df32ee85f7f48a21d93b60a35bea45c817eaa246
Hannes Schippmann <hannes@virtusize.com>
2013-09-10 17:19:28 +0200

    Permission implementation pseudo code
    @jtsoi maybe something like this. Let's talk about it tomorrow. Cheers …

--- tests/flask_nap_tests/fixtures.py
+++ tests/flask_nap_tests/fixtures.py
@@ -7,7 +7,7 @@ from flask_nap.view_filters import CamelizeFilter, ExcludeFilter
 from flask_nap.exception_handlers import UnsupportedMethodExceptionHandler, ModelNotFoundExceptionHandler, ModelInvalidExceptionHandler
 from sa_nap.controller import SAModelController
 from sa_nap.model import SAModelSerializer
-from tests.fixtures import ProductType, ProductTypes, Store, User
+from tests.fixtures import ProductType, ProductTypes, Store, User, Product
 from tests.helpers import db_session
 from nap.model import ModelSerializer
 from nap.controller import ModelController
@@ -16,6 +16,7 @@ from nap.controller import ModelController
 class ProductTypeController(ModelController):
     model = ProductType
     model_storage = ProductTypes
+    #guard = Guard
 
 
 class ProductTypeView(ModelView):
@@ -27,6 +28,7 @@ class ProductTypeView(ModelView):
 class StoreController(SAModelController):
     model = Store
     session_factory = db_session
+    #guard = Guard
 
 
 class StoreView(ModelView):
@@ -38,12 +40,73 @@ class StoreView(ModelView):
 class UserController(SAModelController):
     model = User
     session_factory = db_session
+    #guard = Guard
 
 
 class UserView(ModelView):
     controller = UserController
     filter_chain = [ExcludeFilter(['password']), CamelizeFilter]
     serializer = SAModelSerializer
+
+
+class ProductController(SAModelController):
+    model = Product
+    session_factory = db_session
+    #guard = Guard
+
+
+class ProductView(ModelView):
+    controller = ProductController
+    filter_chain = [CamelizeFilter]
+    serializer = SAModelSerializer
+
+
+###########################
+# Authorization pseudo code
+###########################
+
+"""
+class AdminRole(Role):
+
+    permissions = [
+        StoreController.permissions.manage,
+        ProductController.permissions.manage,
+        UserController.permissions.manage,
+        ProductTypeController.permissions.manage,
+    ]
+
+
+class StoreOwnerRole(Role):
+
+    permissions = [
+        StoreController.permissions.manage,
+        ProductController.permissions.manage,
+        ProductTypeController.permissions.index,
+        ProductTypeController.permissions.read,
+        UserController.permissions.read,
+        UserController.permissions.write,
+    ]
+
+
+class AnonymousRole(Role):
+
+    permissions = [
+        ProductController.permissions.read,
+        ProductTypeController.permissions.index,
+        ProductTypeController.permissions.read,
+    ]
+"""
+
+# Then later somewhere
+"""
+g.identity = Identity([AdminRole])
+"""
+
+# before a controller method is executed the following.
+# Raises exception that is handled by our exception handlers
+"""
+self.guard.can(g.identity, method_name, prefetched_model||None)
+"""
 
 
 class AnApi(Api):
