0b8c2a23e1faced49106941f4b44193bef030e23
Jevgenij Tsoi <jevgenij@virtusize.com>
2013-09-10 22:14:52 +0200

    Some thoughts about auth.

--- nap/authorization.py
+++ nap/authorization.py
@@ -1,16 +1,74 @@
 # -*- coding: utf-8 -*-
+"""
+Hannes,
+
+I think we need an Action class, partly to allow to use non string "actions"
+an partly because we need support for "umbrella" actions such as "manage"
+"moderate" etc...
+
+But we also want to bea able to just use strings to identify actions and
+to allow future users of API to define custom actions.
+
+So I am thinking:
+
+class CRUDActions(ActionsContainer):
+
+    read = Action()
+    create = Action()
+    update = Action()
+    delete = Action()
+
+class ExtendedCRUDActions(CRUDActions):
+
+    manage = Action()
+    manage.include(CRUDActions.read, CRUDActions.write, ...)
+
+    ##Need to test if this kind of inheritance works.
+
+class Roles(object):
+
+    guest = Role()
+    guest.grant('read', Store)
+    guest.grant(CRUDActions.write, 'store')
+
+    admin = Role()
+    admin.inherit(guest)
+    admin.grant('manage', 'store')
+
+
+Then the Guard needs to be initialized with all the roles, and actions.
+
+like this:
+
+guard = Guard(ExtendedCRUDActions, Roles)
+
+or even better:
+
+MyAppGuard(Guard):
+    actions = ExtendedCRUDActions
+    roles = Roles
+
+class MyAPI(Api):
+    guard = MyAppGuard
+
+## Not sure about where to put the guard, maybe on API or controller?!
+## Prolly better on controller, even if it is repetitive.
+
+"""
+
+from inflection import underscore
 
 
 def subject_alias(subject):
     if isinstance(subject, basestring):
-        return subject
+        return underscore(subject)
     elif isinstance(subject, type):
-        return subject.__name__.lower()
+        return underscore(subject.__name__)
     else:
-        return type(subject).__name__.lower()
+        return underscore(type(subject).__name__)
 
 
-class Guard:
+class Guard(object):
     """
     Identity is the base class for users, roles, services, etc.
     Identity provides a set of permissions.
@@ -28,25 +86,26 @@ class Guard:
         return False
 
 
-class Identity:
+class Identity(object):
 
     def __init__(self, roles):
         self.roles = roles
 
 
-class Role:
+class Role(object):
     def __init__(self, *args, **kwargs):
         self.permissions = []
 
     def grant(self, *args, **kwargs):
         self.permissions.append(Permission(*args, **kwargs))
+        return self
 
 
 def true_condition(self, *args, **kwargs):
     return True
 
 
-class Permission:
+class Permission(object):
 
     def __init__(self, action, subject, condition=true_condition):
         self.action = action
